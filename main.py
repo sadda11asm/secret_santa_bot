41a5824194fef90edba55fbe7f2309ecf740fbae
Saddam <saddam.asmatullayev@nu.edu.kz>
2019-11-03 00:20:38 +0600

    set webhooks

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import logging
 
 from telegram.ext import CommandHandler
 import os
+import sys
 
-TOKEN = "AAG05g6L0oqNSGLYyGuGBwDPDdNexGXrFRA"
+MODE = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger()
 
 updater = Updater(TOKEN)
 PORT = int(os.environ.get('PORT', '8443'))
@@ -13,17 +17,30 @@ PORT = int(os.environ.get('PORT', '8443'))
 dispatcher = updater.dispatcher
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+def start_handler(bot, update):
+    logger.info("User {} started bot".format(update.effective_user["id"]))
+    update.message.reply_text("Hello from Python!\nPress /random to get random number")
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+if MODE == "dev":
+    def run(updater):
+        updater.start_polling()
+elif MODE == "prod":
+    def run(updater):
+        PORT = int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://nusecretsantabot.herokuapp.com/" + TOKEN)
+else:
+    logger.error("No MODE specified!")
+    sys.exit(1)
 
+if __name__ == '__main__':
+    logger.info("Starting bot")
+    updater = Updater(TOKEN)
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
 
-updater.start_webhook(listen='0.0.0.0',
-                      port=PORT,
-                      url_path=TOKEN)
+    run(updater)
 
-updater.bot.set_webhook("https://lit-hamlet-25776.herokuapp.com/" + TOKEN)
-updater.idle()
